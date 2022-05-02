6e2ceb37af35d973c848f6d968a4a8222a363579
kosPap1 <konstantinosgpappas@gmail.com>
2022-05-02 12:26:37 +0300

    This includes the XGboost Regressor for the same 4 cases.

---
+++ functions/dataInput.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+# Δεδομένα Φορτίου
+
+load2011 = pd.read_excel('/home/konstantinos/PycharmProject/Xgboost/data/LoadData/load2011.xlsx', names= pd.date_range(start='1/1/2011', periods=365, freq='D'))
+load2012 = pd.read_excel('/home/konstantinos/PycharmProject/Xgboost/data/LoadData/load2012.xlsx', names= pd.date_range(start='1/1/2012', periods=366, freq='D'))
+load2013 = pd.read_excel('/home/konstantinos/PycharmProject/Xgboost/data/LoadData/load2013.xlsx', names= pd.date_range(start='1/1/2013', periods=365, freq='D'))
+load2014 = pd.read_excel('/home/konstantinos/PycharmProject/Xgboost/data/LoadData/load2014.xlsx', names= pd.date_range(start='1/1/2014', periods=365, freq='D'))
+load2015 = pd.read_excel('/home/konstantinos/PycharmProject/Xgboost/data/LoadData/load2015.xlsx', names= pd.date_range(start='1/1/2015', periods=365, freq='D'))
+load2016 = pd.read_excel('/home/konstantinos/PycharmProject/Xgboost/data/LoadData/load2016.xlsx', names= pd.date_range(start='1/1/2016', periods=366, freq='D'))
+load2017 = pd.read_excel('/home/konstantinos/PycharmProject/Xgboost/data/LoadData//load2017.xlsx', names= pd.date_range(start='1/1/2017', periods=365, freq='D'))
+load2018 = pd.read_excel('/home/konstantinos/PycharmProject/Xgboost/data/LoadData//load2018.xlsx', names= pd.date_range(start='1/1/2018', periods=365, freq='D'))
+load2019 = pd.read_excel('/home/konstantinos/PycharmProject/Xgboost/data/LoadData//load2019.xlsx', names= pd.date_range(start='1/1/2019', periods=365, freq='D'))
+
+# Δεδομένα θερμοκρασίας
+
+temp2011 = pd.read_excel('/home/konstantinos/PycharmProject/Xgboost/data/temperatureData/temp2011.xlsx', names=pd.date_range(start='1/1/2011', periods=365, freq='D'))
+temp2012 = pd.read_excel('/home/konstantinos/PycharmProject/Xgboost/data/temperatureData/temp2012.xlsx', names=pd.date_range(start='1/1/2012', periods=366, freq='D'))
+temp2013 = pd.read_excel('/home/konstantinos/PycharmProject/Xgboost/data/temperatureData/temp2013.xlsx', names=pd.date_range(start='1/1/2013', periods=365, freq='D'))
+temp2014 = pd.read_excel('/home/konstantinos/PycharmProject/Xgboost/data/temperatureData/temp2014.xlsx', names=pd.date_range(start='1/1/2014', periods=365, freq='D'))
+temp2015 = pd.read_excel('/home/konstantinos/PycharmProject/Xgboost/data/temperatureData/temp2015.xlsx', names=pd.date_range(start='1/1/2015', periods=365, freq='D'))
+temp2016 = pd.read_excel('/home/konstantinos/PycharmProject/Xgboost/data/temperatureData/temp2016.xlsx', names=pd.date_range(start='1/1/2016', periods=366, freq='D'))
+temp2017 = pd.read_excel('/home/konstantinos/PycharmProject/Xgboost/data/temperatureData/temp2017.xlsx', names=pd.date_range(start='1/1/2017', periods=365, freq='D'))
+temp2018 = pd.read_excel('/home/konstantinos/PycharmProject/Xgboost/data/temperatureData/temp2018.xlsx', names=pd.date_range(start='1/1/2018', periods=365, freq='D'))
+temp2019 = pd.read_excel('/home/konstantinos/PycharmProject/Xgboost/data/temperatureData/temp2019.xlsx', names=pd.date_range(start='1/1/2019', periods=365, freq='D'))
+
---
+++ main.py
@@ -0,0 +1,57 @@
+from days.TheofaniaHoliday import TheofaniaPrediction
+from days.CleanMondayHoliday import CleanMondayPrediction
+from days.RevolutionAnniversary import PredictionRevolutionAnniversary
+from days.GoodFridayHoliday import GoodFridayHoliday
+from days.EasterSunday import easterSunday
+from days.EasterMondayHoliday import easterMonday
+from days.LabourDayHoliday import labourDay
+from days.PentikostiHoliday import pentikosti
+from days.HolySpiritHoliday import holySpirit
+from days.DormitionOfTheMotherOfGodHoliday import august15
+from days.OXIdayHoliday import oxiDay
+from days.ChristmasDayHoliday import christmasDay
+from days.secondDayofChristmas import christmas2ndDay
+from days.NewYear import newYear
+
+
+# Prediction For Theofania Holiday
+TheofaniaPrediction()
+
+# Prediction for Clean Monday
+CleanMondayPrediction()
+
+# Prediction for Revolution Anniversary
+PredictionRevolutionAnniversary()
+
+# Prediction for Good Friday
+GoodFridayHoliday()
+
+# Prediction for Easter Sunday
+easterSunday()
+
+# Prediction for Easter Monday
+easterMonday()
+
+# Prediction for Labour Day
+labourDay()
+
+# Prediction for Pentikosti
+pentikosti()
+
+# Prediction for Holy Spirit Holiday
+holySpirit()
+
+#Prediction for Dormition of The mother of God
+august15()
+
+#Prediction for Oxi Day
+oxiDay()
+
+#Prediction for Christmas Day
+christmasDay()
+
+#Prediction for 2nd Day of Christmas
+christmas2ndDay()
+
+#Prediction for New Year
+newYear()
